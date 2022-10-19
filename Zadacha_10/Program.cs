2536aded24249f65b3cb9dc432507a3909c7735f
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-19 20:54:14 +0400

    Izm folder Zadacha10

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -2,16 +2,26 @@
 // и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine("Введите число:");
-int num = Convert.ToInt32(Console.ReadLine());
-// for (;num<100 || num>999; num=Convert.ToInt32(Console.ReadLine())) Console.WriteLine("Число должно быть трехзначным");
-if (num < 100 || num > 999)
-{
-    Console.WriteLine("Число должно быть трехзначным.");
-}
+int num1 = Convert.ToInt32(Console.ReadLine());
+//for (;num<100 || num>999; num=Convert.ToInt32(Console.ReadLine())) Console.WriteLine("Число должно быть трехзначным");
 
-if (num >= 100 || num <=999)   //else
+if (num1 >= 100 && num1 <=999)   //else
 {
-    int a1 = num / 10;
+    int a1 = num1 / 10;
     int a2=a1%10;
     Console.WriteLine("Вторая цифра введенного числа: " + a2);
 }
+
+if (num1 < 100 || num1 > 999)
+{
+    Console.WriteLine("Число должно быть трехзначным. Введите трехзначное число:");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int a3 = num2 / 10;
+    int a4=a3%10;
+    Console.WriteLine("Вторая цифра введенного числа: " + a4);
+}
+
+//int a1 = num / 10;
+//int a2=a1%10;
+//Console.WriteLine("Вторая цифра введенного числа: " + a2);
+
