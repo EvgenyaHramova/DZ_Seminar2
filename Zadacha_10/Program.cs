77a0e5d0e29493b7327e3d4b455ddb1decc5ef98
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-19 20:25:58 +0400

    Izm folder Zadacha10

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -3,8 +3,15 @@
 
 Console.WriteLine("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
-for (;num<100 || num>999; num=Convert.ToInt32(Console.ReadLine())) Console.WriteLine("Число должно быть трехзначным");
-//Console.Write(num);
+// for (;num<100 || num>999; num=Convert.ToInt32(Console.ReadLine())) Console.WriteLine("Число должно быть трехзначным");
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Число должно быть трехзначным.");
+}
 
-//int a2 = num / 10;
-//Console.WriteLine("Вторая цифра введенного числа:" + {a2});
+if (num >= 100 || num <=999)   //else
+{
+    int a1 = num / 10;
+    int a2=a1%10;
+    Console.WriteLine("Вторая цифра введенного числа: " + a2);
+}
