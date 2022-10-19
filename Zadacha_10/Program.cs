ca4cae09b91947394a823bfd9b6ed8dd13a068fa
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-19 21:04:54 +0400

    Izm folder Zadacha10

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -1,14 +1,13 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число
-// и на выходе показывает вторую цифру этого числа.
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Введите число:");
+Console.WriteLine("Введите трехзначное число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
-//for (;num<100 || num>999; num=Convert.ToInt32(Console.ReadLine())) Console.WriteLine("Число должно быть трехзначным");
 
-if (num1 >= 100 && num1 <=999)   //else
+
+if (num1 >= 100 && num1 <= 999)
 {
     int a1 = num1 / 10;
-    int a2=a1%10;
+    int a2 = a1 % 10;
     Console.WriteLine("Вторая цифра введенного числа: " + a2);
 }
 
@@ -17,11 +16,6 @@ if (num1 < 100 || num1 > 999)
     Console.WriteLine("Число должно быть трехзначным. Введите трехзначное число:");
     int num2 = Convert.ToInt32(Console.ReadLine());
     int a3 = num2 / 10;
-    int a4=a3%10;
+    int a4 = a3 % 10;
     Console.WriteLine("Вторая цифра введенного числа: " + a4);
 }
-
-//int a1 = num / 10;
-//int a2=a1%10;
-//Console.WriteLine("Вторая цифра введенного числа: " + a2);
-
