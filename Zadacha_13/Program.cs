1ed1f0053e3587d478b2f48999c3bcd6a5c5ea3e
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-20 14:34:09 +0400

    New folder Zadacha13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < 100 && num1 > -100)
+{
+    Console.WriteLine("Третьей цифры в числе нет.");
+}
+
+else
+{
+    while (num1 > 999)
+    {
+        num1 = num1 / 10;
+    }
+
+    int a1 = num1 % 10;
+
+    Console.WriteLine("Третья цифра введенного числа: " + Math.Abs(a1));
+}
